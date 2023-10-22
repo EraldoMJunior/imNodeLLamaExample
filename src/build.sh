2e60d4df4bed2886dc728929e10e5966fabb9aa8
EraldoMJunior <117386914+EraldoMJunior@users.noreply.github.com>
2023-10-22 11:40:21 -0300

    Create build.sh

---
+++ src/build.sh
@@ -0,0 +1,1 @@
+c++ `sdl2-config --cflags` -I .. -I ../.. -I ../../backends main.cpp ../../backends/imgui_impl_sdl2.cpp ../../backends/imgui_impl_opengl2.cpp ../../imgui*.cpp `sdl2-config --libs` -lGL
