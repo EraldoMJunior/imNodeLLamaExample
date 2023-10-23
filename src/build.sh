7cc72bbe67c7f669a08f4c9e2a163f79ec356ee8
Eraldo Machado Rangel Jr <eraldomr@motorola.com>
2023-10-23 13:43:34 -0300

    early stages demo test

--- src/build.sh
+++ src/build.sh
@@ -1,1 +1,4 @@
-c++ `sdl2-config --cflags` -I .. -I ../.. -I ../../backends main.cpp ../../backends/imgui_impl_sdl2.cpp ../../backends/imgui_impl_opengl2.cpp ../../imgui*.cpp `sdl2-config --libs` -lGL
+#assign imgui folder as environment variable
+export IMGUI_DIR=../imgui
+
+c++ `sdl2-config --cflags` -I$IMGUI_DIR -I$IMGUI_DIR/backends main.cpp $IMGUI_DIR/backends/imgui_impl_sdl2.cpp $IMGUI_DIR/backends/imgui_impl_opengl2.cpp $IMGUI_DIR/imgui*.cpp `sdl2-config --libs` -ldl -lGL
